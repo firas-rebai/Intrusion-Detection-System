9efa6a1abc41c3b1f34693333d64b4f8b8a2c522
firas <firasrebai01@gmail.com>
2023-12-10 21:34:43 +0100

    test

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
