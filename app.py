14c5c65b91de0caa26369b7aaed7c4b08b25dde9
firas <firasrebai01@gmail.com>
2023-12-11 23:13:18 +0100

    production #2

--- app.py
+++ app.py
@@ -30,7 +30,58 @@ class NetworkConnection:
         self.dst_host_serror_rate, self.dst_host_srv_serror_rate, self.dst_host_rerror_rate, \
         self.dst_host_srv_rerror_rate = args
 
+attributes = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment',
+              'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted',
+              'num_root', 'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds',
+              'is_host_login', 'is_guest_login', 'count', 'srv_count', 'serror_rate', 'srv_serror_rate',
+              'rerror_rate', 'srv_rerror_rate', 'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate',
+              'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
+              'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
+              'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
+@app.route('/')
+def index():
+    return render_template('home.html', attributes=attributes)
 
+@app.route('/process', methods=['POST'])
+def process():
+
+    values_list = [request.form[attr] for attr in attributes]
+    columns = (
+        ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment', 'urgent',
+         'hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted'
+            , 'num_root', 'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds', 'is_host_login',
+         'is_guest_login', 'count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
+         'same_srv_rate', 'diff_srv_rate'
+            , 'srv_diff_host_rate', 'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate',
+         'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
+         'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate'])
+    data = pd.DataFrame([values_list], columns=columns)
+    data.drop(columns=["num_outbound_cmds"], inplace=True)
+    for col in ['protocol_type', 'service', 'flag']:
+        data[col] = data[col].astype('category')
+    label_encoder = LabelEncoder()
+
+    for col in data.select_dtypes(include="category").columns:
+        data[col] = label_encoder.fit_transform(data[col])
+
+    predictions = [forest.predict(data.iloc[:, :12]), knn.predict(data.iloc[:, :12]),
+                   adaboost.predict(data.iloc[:, :12])]
+    results = []
+    for pred in predictions:
+        print(pred[0])
+        if pred[0] == 0:
+            results.append("Normal")
+        if pred[0] == 1:
+            print("test")
+            results.append("DoS")
+        if pred[0] == 2:
+            results.append("U2R")
+        if pred[0] == 3:
+            results.append("R2L")
+        if pred[0] == 4:
+            results.append("Probe")
+    print(results[0])
+    return render_template("result.html", forest=results[0], knn=results[1], adaboost=results[2])
 @app.route('/test/<values>')
 def test(values):  # put application's code here
     values_list = values.split(',')
